a1ab1fa4e768d8810ea3e7888331cfb2939aa16e
Nanna Hansen <nann257j@stud.kea.dk>
2022-08-25 15:22:25 +0200

    egne farver

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      farve: "#F1F5F9",
+      farve2: "#1E293B",
+      farve3: "#DCE2F2",
+      farve4: "#626B73",
+      farve5: "#475945",
+      farve6: "#ACBFA3",
+      farve7: "#808C56",
+      farve8: "#FFFFFF",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
